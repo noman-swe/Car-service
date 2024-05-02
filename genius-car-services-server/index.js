19142c801e3e007b5eed67ea01c96dcf3373cfcb
noman-swe <noman.swe130@gmail.com>
2024-05-02 12:39:56 +0600

    DB Connection set-up has done with .env protected DB_USER & DB_PASS

--- genius-car-services-server/index.js
+++ genius-car-services-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 // middlwares
@@ -7,8 +8,39 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json())
 
+// connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmq0nwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+/*  */
+
+
+// 
 app.get('/', (req, res) => {
     res.send('Running my genius car server.')
 })
+app.listen(port, () => { console.log("Running on port", port) });
 
-app.listen(port, () => { console.log("Running on port", port) })
